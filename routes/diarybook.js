ff3097d2fa90e9f4ce2f9a4c11a664049d9b161e
chenannchi <anchichenusa@gmail.com>
2023-01-20 22:36:01 +0800

    set up the router and controller file for diarybook

---
+++ routes/diarybook.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import * as diarybookCtrl from '../controllers/diarybook.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+// ========== Public Routes ===========
+
+
+// ========= Protected Routes ========= 
+router.use(decodeUserFromToken)
+
+
+export { router }
