ba8db6cd6997566616f1a9461d6687f7649bd4f4
chenannchi <anchichenusa@gmail.com>
2023-01-22 00:25:42 +0800

    implement show functionality to diary

--- controllers/diaries.js
+++ controllers/diaries.js
@@ -29,7 +29,18 @@ const index = async (req, res) => {
   }
 }
 
+const show = async (req, res) => {
+  try {
+    const diary = await Diary.findById(req.params.id)
+      .populate('author')
+    res.status(200).json(diary)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 export {
   create,
   index,
+  show,
 }
--- routes/diaries.js
+++ routes/diaries.js
@@ -11,5 +11,6 @@ const router = Router()
 router.use(decodeUserFromToken)
 router.post('/', checkAuth, diariesCtrl.create)
 router.get('/', checkAuth, diariesCtrl.index)
+router.get('/:id', diariesCtrl.show)
 
 export { router }
